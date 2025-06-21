d03d2af9103b5741447c22022bf3dcedf3cb6829
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-06-21 14:03:23 +0530

    Initial commit

---
+++ Arrays-1/3-Kadane's Algo.cpp
@@ -0,0 +1,27 @@
+#include <climits>
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
