0b02a12f5731d2d739dd9b4ed74758b4cc329463
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-06-21 14:58:37 +0530

    Buy and Sell Stocks

---
+++ Arrays-1/5-BuySellStocks.cpp
@@ -0,0 +1,40 @@
+// Approach -1 : N^2
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+            maxPro = max(arr[j] - arr[i], maxPro);
+            }
+        }
+        }
+    }
+
+    // Approach - 2 : Linear
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+    
