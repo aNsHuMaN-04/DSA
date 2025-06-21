72f0ade0fb25387eb3984d481e025ddbdaa1640f
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-06-21 14:41:20 +0530

    Dutch National FLag

---
+++ Arrays-1/4-SortColours.cpp
@@ -0,0 +1,42 @@
+// Approach - 1 : Count the number of 0, 1, and 2s
+
+
+
+//Aproach - 2 : Dutch National Flag Algo 
+
+
+// arr[0….low-1] contains 0. [Extreme left part]
+// arr[low….mid-1] contains 1.
+// arr[high+1….n-1] contains 2. [Extreme right part], n = size of the array
+// The middle part i.e. arr[mid….high] is the unsorted segment.
+
+// If arr[mid] == 0, we will swap arr[low] and arr[mid] and will increment both low and mid. Now the subarray from index 0 to (low-1) only contains 0.
+// If arr[mid] == 1, we will just increment the mid pointer and then the index (mid-1) will point to 1 as it should according to the rules.
+// If arr[mid] == 2, we will swap arr[mid] and arr[high] and will decrement high. Now the subarray from index high+1 to (n-1) only contains 2.
+// In this step, we will do nothing to the mid-pointer as even after swapping,
+// the subarray from mid to high(after decrementing high) might be unsorted.
+// So, we will check the value of mid again in the next iteration.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(vector<int>& arr, int n) {
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
