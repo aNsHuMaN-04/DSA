14a856065c2c6f7d7581a0cf1c7d76234d7026f7
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-06-27 21:37:19 +0530

    Combination Sum 1

---
+++ Recursion-1/3-CombinationSum1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int>& nums, int target, int i, vector<int>& ds, vector<vector<int>>& ans) {
+    if (i == nums.size()) {
+        if (target == 0)
+            ans.push_back(ds);
+        return;
+    }
+
+    if (nums[i] <= target) {
+        ds.push_back(nums[i]);
+        solve(nums, target - nums[i], i, ds, ans);
+        ds.pop_back();
+    }
+
+    solve(nums, target, i + 1, ds, ans);
+}
+
+vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<int> ds;
+    vector<vector<int>> ans;
+    solve(nums, target, 0, ds, ans);
+    return ans;
+}
