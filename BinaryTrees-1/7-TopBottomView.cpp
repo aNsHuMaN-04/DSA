d7d81cb6a85fc840513155e2ee2cdc3150d4a65f
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-06-29 15:24:04 +0530

    Bottom View

--- BinaryTrees-1/7-TopBottomView.cpp
+++ BinaryTrees-1/7-TopBottomView.cpp
@@ -42,3 +42,39 @@ vector<int> topView(TreeNode* root) {
 
     return ans;
 }
+
+
+
+vector<int> bottomView(TreeNode* root) {
+    vector<int> ans;
+    if (root == NULL) return ans;
+
+    map<int, int> mpp;               // <distance, node value>
+    queue<pair<int, TreeNode*> > q;  // <distance, node>
+
+    q.push(make_pair(0, root));
+
+    while (!q.empty()) {
+        pair<int, TreeNode*> temp = q.front();
+        q.pop();
+
+        int line = temp.first;
+        TreeNode* node = temp.second;
+
+        // Always update for bottom view
+        mpp[line] = node->val;
+
+        if (node->left != NULL) {
+            q.push(make_pair(line - 1, node->left));
+        }
+        if (node->right != NULL) {
+            q.push(make_pair(line + 1, node->right));
+        }
+    }
+
+    for (auto it : mpp) {
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
