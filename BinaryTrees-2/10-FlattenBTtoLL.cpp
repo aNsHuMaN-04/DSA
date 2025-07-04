91ef1aab6b66f41050ce07fe2e4ea4d6d12e3ae8
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-07-04 14:04:00 +0530

    Flatten a BT to LL

---
+++ BinaryTrees-2/10-FlattenBTtoLL.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Definition for a binary tree node.
+ 
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+  
+ 
+ void flatten(TreeNode* root) {
+        TreeNode* prev = NULL;
+        solve(root, prev);
+    }
+
+    void solve(TreeNode* root, TreeNode*& prev) {
+        if (root == NULL)
+            return;
+
+        solve(root->right, prev);
+
+        solve(root->left, prev);
+
+        root->right = prev;
+        root->left = NULL;
+
+        prev = root;
+    }
