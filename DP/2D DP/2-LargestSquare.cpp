a6013c8c6941f3a94951f49e522ea700b39cd9d2
Anshuman Kapoor <anshumansimpy@gmail.com>
2025-07-16 22:23:56 +0530

    Memoisation

--- DP/2D DP/2-LargestSquare.cpp
+++ DP/2D DP/2-LargestSquare.cpp
@@ -21,9 +21,38 @@ int solve(vector<vector<int>> mat, int i, int j, int& maxi)
         return 0;
 }
 
-int MaxSquare(vector<vector<int>> mat)
+int MaxSquare(vector<vector<int>> mat, int n, int m)
 {
     int maxi = 0;
     solve(mat, 0, 0, maxi);
     return maxi;
+
+
+    int maxi = 0;
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    solveMem(mat, 0, 0, maxi, dp);
+    return maxi;
+}
+
+int solveMem(vector<vector<int>> mat, int i, int j, int& maxi, vector<vector<int>>& dp)
+{
+    if(i >= mat.size() || j >= mat[0].size())
+        return 0;
+
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    int right = solveMem(mat, i, j + 1, maxi, dp);
+    int diagonal = solveMem(mat, i + 1, j + 1, maxi, dp);
+    int down = solveMem(mat, i + 1, j, maxi, dp);
+
+    if(mat[i][j] == 1)
+    {
+        dp[i][j] = 1 + min(right, min(diagonal, down));
+        maxi = max(maxi, dp[i][j]);
+        return dp[i][j];
+    }
+
+    else
+        return dp[i][j] = 0;
 }
